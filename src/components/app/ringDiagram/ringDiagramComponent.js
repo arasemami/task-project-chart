dca8c827b3ce018872831c24b1ef747ca1a3114a
aras armani <aras.armani2014@gmail.com>
2019-07-30 20:25:07 +0300

    fixed cleancode

--- src/components/app/ringDiagram/ringDiagramComponent.js
+++ src/components/app/ringDiagram/ringDiagramComponent.js
@@ -14,19 +14,22 @@ class RingDiagramComponent extends Component {
             <div className="ring-container" key={this.props.name}>
                 <h2>{this.props.name}</h2>
                 <p className="label-of-ring">{this.props.data} {this.props.extension}</p>
-                <CircularProgressbar value={(this.props.data * 100) / this.props.maxData}
-                                     text={`${parseInt((this.props.data * 100) / this.props.maxData)}%`}
-                                     strokeWidth={17}
-                                     styles={buildStyles({
-                                         rotation: 0.25,
-                                         strokeLinecap: 'butt',
-                                         textSize: '16px',
-                                         pathTransitionDuration: 0.5,
-                                         pathColor: `rgba(${this.props.color}, ${this.props.data / 10})`, // make color becuse in vlaue of data
-                                         textColor: '#f88',
-                                         trailColor: '#d6d6d6',
-                                         backgroundColor: this.props.color,
-                                     })}
+                <CircularProgressbar 
+                        value={(this.props.data * 100) / this.props.maxData}
+                        text={`${parseInt((this.props.data * 100) / this.props.maxData)}%`}
+                        strokeWidth={17}
+                        styles={
+                            buildStyles({
+                                rotation: 0.25,
+                                strokeLinecap: 'butt',
+                                textSize: '16px',
+                                pathTransitionDuration: 0.5,
+                                pathColor: `rgba(${this.props.color}, ${this.props.data / 10})`, // make color becuse in vlaue of data
+                                textColor: '#f88',
+                                trailColor: '#d6d6d6',
+                                backgroundColor: this.props.color,
+                            })
+                        }
                 />
             </div>
         );
