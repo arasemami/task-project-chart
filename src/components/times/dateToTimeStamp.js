9a001d58cf3bc54aba72de77ad9699c7972a5dfb
aras armani <aras.armani2014@gmail.com>
2019-07-31 14:54:13 +0300

    fixed wirt test for date convertor to test

--- src/components/times/dateToTimeStamp.js
+++ src/components/times/dateToTimeStamp.js
@@ -2,11 +2,11 @@
     
     function timeStamp(val){
 
-            var datum = Date.parse(val);
-            var timestamp = datum/1000;
-            var a = new Date(timestamp*1000);
-            var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-            var dayOfWeek = days[a.getDay()]
+            let datum = Date.parse(val);
+            let timestamp = datum/1000;
+            let a = new Date(timestamp*1000);
+            let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+            let dayOfWeek = days[a.getDay()]
 
                 return  dayOfWeek;  
     }
---
+++ src/components/times/dateToTimeStamp.test.js
@@ -0,0 +1,11 @@
+
+
+
+import timeStamp from './dateToTimeStamp';
+
+
+test ('timeStamp', () => {
+    expect(timeStamp('2019/01/22')).toBe('Tuesday');
+    expect(timeStamp('2019/07/31')).toBe('Wednesday');
+
+})
